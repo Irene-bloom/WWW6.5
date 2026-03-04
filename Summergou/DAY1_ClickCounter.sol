49a283b2a7d6ca621193776f0fe27c9d64c76f84
gousummer <gousummer@gmail.com>
2026-03-04 17:18:23 +0800

    Update DAY1_ClickCounter.sol

--- Summergou/DAY1_ClickCounter.sol
+++ Summergou/DAY1_ClickCounter.sol
@@ -1,11 +1,14 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier:MIT
+
 pragma solidity ^0.8.0;
 
-contract ClickCounter {
-    uint256 public counter;
+contract ClickCounter{
 
-    function click() public {
-        counter++;
+    uint256 public counter ;
+
+    function click() public 
+    {
+        counter ++;
     }
 }
 
